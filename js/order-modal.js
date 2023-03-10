4e8a00243ffb644a55c5bd266367a9088bedeba8
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-03-10 19:56:12 +0900

    🚧 Implement toggling bookmark-button

--- js/order-modal.js
+++ js/order-modal.js
@@ -16,3 +16,35 @@ function closeOrderModal() {
 
 orderCtaBuyButton.addEventListener('click', openOrderModal)
 orderModalOverlay.addEventListener('click', closeOrderModal)
+
+function toggleOrderCtaBookmark() {
+  // 1. 버튼 클래스 + is-active
+  // 2. icon 클래스 -> ic-bookmark-filled
+  // 3. 카운트 숫자 +1
+
+  const [icon, countSpan] = this.children
+  const count = Number(countSpan.innerHTML.replaceAll(',', ''))
+  let newCount = count
+
+  console.log(typeof count)
+
+  if (this.classList.contains('is-active')) {
+    // 비활성화
+    icon.classList.add('ic-bookmark')
+    icon.classList.remove('ic-bookmark-filled')
+    newCount = newCount - 1
+    console.log(newCount)
+  } else {
+    // 활성화
+    icon.classList.remove('ic-bookmark')
+    icon.classList.add('ic-bookmark-filled')
+    newCount = newCount + 1
+    console.log(newCount)
+  }
+
+  countSpan.innerHTML = newCount.toLocaleString()
+  countSpan.setAttribute('aria-label', `북마크 ${newCount.toLocaleString()}회`)
+  this.classList.toggle('is-active')
+}
+
+orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark)
