e885d079f4795c1418e232ff21502b0c3707cac6
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-03-13 18:03:20 +0900

    🚧 Detect position of tab-panel

--- js/product-tab.js
+++ js/product-tab.js
@@ -34,3 +34,45 @@ productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab)
   button.addEventListener('click', scrollToTabpanel)
 })
+
+// 1. window.scrollY : 문서 시작점에서부터 스크롤 한 양 값
+// 2. element.getBoundingClientRect().top : viewport의 시작점에서부터 요소까지의 거리 -> scroll시 요소간의 거리가 달라지므로, element.getBoundingClientRect().top 값도 달라진다
+// 3. element.offsetTop : 문서 시작점 기준, element 까지의 거리
+// 4. element.getBoundingClientRect().top + window.scrollY  === element.offsetTop
+
+const productTabPanelIdList = [
+  'product-spec',
+  'product-review',
+  'product-inquiry',
+  'product-shipment',
+  'product-recommendation',
+]
+
+// map vs forEach
+// 배열 안의 element 각각을 돈다는 점에서 같음
+// map 의 경우, 어떠한 값을 꼭 리턴해줌
+// 그리고 return된 값이 모여서 새로운 배열을 형성함
+
+const productTabPanelList = productTabPanelIdList.map((panelId) => {
+  const tabPanel = document.querySelector(`#${panelId}`)
+  return tabPanel
+})
+
+productTabPanelPositionMap = {}
+
+function detactTabPanelPosition() {
+  productTabPanelList.forEach((panel) => {
+    // 1. 각각의 tabpanel의 id와 y축 위치를 찾는다.
+    const id = panel.getAttribute('id')
+    const position = panel.offsetTop
+
+    // 2. productTabPanelPositionMap에 그 값을 업데이트
+    productTabPanelPositionMap[id] = position
+  })
+}
+
+// window html 요소들이 싹 다 load 된 다음에 실행
+// - 정확한 위치값을 얻기 위해, load 이후에 detactTabPanelPosition 실행
+// resize시에도 정확한 위치값을 얻기 위해 resize 이후 detactTabPanelPosition 실행
+window.addEventListener('load', detactTabPanelPosition)
+window.addEventListener('resize', detactTabPanelPosition)
