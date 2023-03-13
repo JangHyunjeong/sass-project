cce07cfd85af60bb8e89d595b70ffa9a17aa1a9f
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-03-13 19:05:30 +0900

    🐛 Fix bugs on scroll event handler

--- js/product-tab.js
+++ js/product-tab.js
@@ -3,18 +3,26 @@ const productTabButtonList = productTab.querySelectorAll('button')
 
 let TOP_HEADER_DESKTOP = 80 + 50 + 54
 let TOP_HEADER_MOBILE = 50 + 40 + 40
-
 let currentActiveTab = productTab.querySelector('.is-active')
+let disableUpdating = false
 
 function toggleActiveTab() {
   // 1. tab-item 클래스: is-active
   const tabItem = this.parentNode
 
   if (currentActiveTab !== tabItem) {
+    // 추가 ) 탭 클릭시, acitve과정 생략
+    disableUpdating = true
+
     tabItem.classList.add('is-active')
     currentActiveTab.classList.remove('is-active')
     currentActiveTab = tabItem
   }
+
+  // 추가 ) 생략된 tab active과정 되살리기
+  setTimeout(() => {
+    disableUpdating = false
+  }, 1000)
 }
 
 function scrollToTabpanel() {
@@ -50,7 +58,7 @@ const productTabPanelList = productTabPanelIdList.map((panelId) => {
 
 productTabPanelPositionMap = {}
 
-function detactTabPanelPosition() {
+function detectTabPanelPosition() {
   productTabPanelList.forEach((panel) => {
     // 1. 각각의 tabpanel의 id와 y축 위치를 찾는다.
     const id = panel.getAttribute('id')
@@ -59,9 +67,16 @@ function detactTabPanelPosition() {
     // 2. productTabPanelPositionMap에 그 값을 업데이트
     productTabPanelPositionMap[id] = position
   })
+
+  updateActiveTabOnScroll()
 }
 
 function updateActiveTabOnScroll() {
+  // 탭 클릭시, acitve과정 생략
+  if (disableUpdating) {
+    return
+  }
+
   const scrolledAmount =
     window.scrollY +
     (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP + 80 : TOP_HEADER_MOBILE + 8)
@@ -85,17 +100,28 @@ function updateActiveTabOnScroll() {
     newActiveTab = productTabButtonList[0]
   }
 
-  // 활성화된 탭버튼 변경
+  // 추가 : 끝까지 스크롤을 한 경우, currentActiveTab = productTabButtonList[4]
+  // window.scrollY + window.innerHeight === body의 전체 height
+  // window.innerWidth < 1200 - orderCta, 56px
+  const bodyHeight =
+    document.body.offsetHeight + (window.innerWidth > 1200 ? 0 : 56)
+  if (window.scrollY + window.innerHeight === bodyHeight) {
+    newActiveTab = productTabButtonList[4]
+  }
+
+  // 활성화된 탭버튼
   if (newActiveTab) {
     newActiveTab = newActiveTab.parentNode
     if (newActiveTab !== currentActiveTab) {
       newActiveTab.classList.add('is-active')
-      currentActiveTab.classList.remove('is-active')
+      if (currentActiveTab !== null) {
+        currentActiveTab.classList.remove('is-active')
+      }
       currentActiveTab = newActiveTab
     }
   }
 }
 
-window.addEventListener('load', detactTabPanelPosition)
-window.addEventListener('resize', detactTabPanelPosition)
+window.addEventListener('load', detectTabPanelPosition)
+window.addEventListener('resize', detectTabPanelPosition)
 window.addEventListener('scroll', updateActiveTabOnScroll)
