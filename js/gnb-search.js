31bd17b9570f1ad3747dccf3c54e0190f0f51df4
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-03-13 14:36:02 +0900

    🚧 Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -5,11 +5,16 @@ const deleteAllButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 )
 const gnbSearchHistoryList = gnbSearch.querySelector('ol')
+const deleteButtonList = gnbSearchHistoryList.querySelectorAll('.delete-button')
 
-function closeGnbSearchHistory(e) {
+function closeGnbSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside)
+}
+
+function closeGnbSearchHistoryOnClickingOutside(e) {
   if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active')
-    window.removeEventListener('click', closeGnbSearchHistory)
+    closeGnbSearchHistory()
   }
 }
 
@@ -19,15 +24,28 @@ function openGnbSearchHistory() {
   }
 
   if (!gnbSearchHistory.classList.contains('is-active')) {
-    window.addEventListener('click', closeGnbSearchHistory)
+    window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside)
   }
   gnbSearchHistory.classList.add('is-active')
 }
 
 function deleteAllHistoryItems() {
   gnbSearchHistoryList.innerHTML = ''
-  gnbSearchHistory.classList.remove('is-active')
+  closeGnbSearchHistory()
+}
+
+function deleteSearchHistoryItem(e) {
+  e.stopPropagation() // 이벤트 전파 방지 (closeGnbSearchHistoryOnClickingOutside 실행 방지)
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+
+  if (gnbSearchHistoryList.children.length === 0) {
+    closeGnbSearchHistory()
+  }
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
 deleteAllButton.addEventListener('click', deleteAllHistoryItems)
+deleteButtonList.forEach((button) =>
+  button.addEventListener('click', deleteSearchHistoryItem)
+)
